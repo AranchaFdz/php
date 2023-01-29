987ebc830fc7fdbac58601ff2ca070be9b255cd1
Arancha Fernandez <110049521+AranchaFdz@users.noreply.github.com>
2023-01-29 21:29:54 +0100

    file eight done

---
+++ eight.php
@@ -0,0 +1,34 @@
+<?php
+
+// comparisons booleans (true or false)
+
+//echo true; // = "1"
+//echo false; // = "0"
+
+// numbers
+//echo 5 < 10; //true
+//echo 5 > 10; //false
+//echo 5 == 10; //false
+//echo 10 == 10; //true
+//echo 5 != 10; // true
+//echo 5 <= 5; //true
+//echo 5 >= 5; //true
+
+//strings
+//echo'shaun' < 'yoshi'; //true because the s comes before the y in the alphabet
+//echo 'yoshi' > 'shaun'; // false because y does not come before the s in the alphabet
+//echo 'shaun' > 'Shaun'; // true an uppercase letter is seen less than a lowercase letter
+//echo 'mario' == 'mario'; //true they are the same 
+//echo 'mario' == 'Mario'; // false the uppercase letter is less than the lowercase letter
+
+
+//loose vs strict equal comparison
+//echo 5 == '5'; //loose comparison - true although they are an integer and a string
+//echo 5 === '5'; //strict comparison - false because they are different types od data one is an integer and the other is a string
+//echo 5 === 5; // true due to that the data type that we are passing are both exactly the same  
+
+//echo true == '1'; //true
+//echo false == ""; //true
+
+
+?>
